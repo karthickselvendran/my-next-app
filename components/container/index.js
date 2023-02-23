007060f946776cd858500d8eb208db5bd7c916b4
karthickselvendran <selvendrankarthick@gmail.com>
2023-02-23 10:12:37 +0530

    Head added

--- components/container/index.js
+++ components/container/index.js
@@ -1,8 +1,16 @@
+import Head from "next/head";
 import React from "react";
 import styles from "./container.module.css";
 
 const Container = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>My Next Bolg</title>
+      </Head>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
